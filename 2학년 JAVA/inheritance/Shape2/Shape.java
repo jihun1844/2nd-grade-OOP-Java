acbed8510e2c68a9acb65b7d35ed953875b7ba43
jihun1844 <108657822+jihun1844@users.noreply.github.com>
2023-04-03 14:12:20 +0900

    Add files via upload

---
+++ 2학년 JAVA/inheritance/Shape2/Shape.java
@@ -0,0 +1,12 @@
+package inheritance.Shape2;
+
+public class Shape {
+  private int x, y;
+
+  public Shape(int x, int y) {
+    System.out.println("Shape()");
+    this.x = x;
+    this.y = y;
+
+  }
+}
