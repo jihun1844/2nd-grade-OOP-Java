fe0e8c8b1ded796e823b8be055477cd6d92487ce
jihun <tohka113@naver.com>
2023-04-18 20:51:44 +0900

    Add exFile

---
+++ 예제/ArrayStd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+import javax.swing.undo.StateEdit;
+
+public class ArrayStd {
+  public void getValue(int[] array) {
+    Scanner scn = new Scanner(System.in);
+    for (int i = 0; i < array.length; i++) {
+      System.out.println("성적을 입력 하시요");
+      array[i] = scn.nextInt();
+    }
+  } 
+
+  public double getAverage(int[] array) {
+    double total = 0;
+    for (int i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    return total / array.length;
+  }
+
+}
