865201491faff872bce79c35110c1ea751485b6e
jihun <tohka113@naver.com>
2023-06-01 20:43:01 +0900

    0601

--- 2학년 JAVA/lambda/test.java
+++ 2학년 JAVA/lambda/test.java
@@ -8,7 +8,7 @@ public class test {
   public static void main(String[] args) {
     Person p1 = new Person("홍길동", LocalDate.of(2000, 11, 20), "aaaa@gmali.com", Sex.MALE);
     Person p2 = new Person("이지매", LocalDate.of(1990, 7, 10), "bbbb@gmali.com", Sex.FEMALE);
-    Person p3 = new Person("삼지매", LocalDate.of(2005, 3, 8), "cccc@gmali.com", Sex.MALE);
+    Person p3 = new Person("삼지매", LocalDate.of(2000, 3, 8), "cccc@gmali.com", Sex.FEMALE);
 
     //list는 인터페이스, 이 인터페이스를 구현하는 클래스로
     //ArrayList, LinkedList 등이 있다.
@@ -18,8 +18,54 @@ public class test {
     list.add(p2);
     list.add(p3);
 
-    printPersonOlderThen(list, 20, 50);
-    printPerson(list, new CheckPersonFemale10to20());
+    //printPersonOlderThen(list, 20, 50);
+
+    //checkPerson 인터페이스를 implements 하는 클래스를 하나 생성해서
+    //printPersons 메소드를 호출해라
+    //단 이 메소드의 의해 출력되는 Person 객체는 성별이 남자인 객체여야한다
+    //printPerson(list, new CheckPersonMale());
+
+    //위와 같이 성별이 남자인 객체만 출력하도록
+    //printPerson 메서드를 호출하되, 무명클래스로 작성 
+    //CheckPerson 인터페이스를 구현하는 무명클래스를 생성해 printPerson 메서드를 호출하면 됨
+    // printPerson(list, new CheckPerson() {
+    //   public boolean test(Person p) {
+    //     return p.getGender() == Sex.MALE;
+    //   }
+    // });
+
+    /*
+     람다식은 이름없는 메소드라 할수 있다
+     람다식은 이용하는 이유는 간단하기 때문이다
+     람다식을 이용하는 메소드가 필요한 곳에 간단히 메소드를 보낼수 있다
+     람다식은 함수형 프로그래밍을 자바에 도입한 것이다
+     람다식은 오직 하나의 추상 메소드를 가지는 인터페이스 타입의 객체를 요구하는 메소드의 인자로 전달될수 있다
+     람다식의 문법
+     (arg1, arg2, ...) -> {body}
+     (type1 arg1, type2 arg2, ...) -> {body}
+     */
+
+    //위의 무명클래스를 람다식으로 구현 해보자
+    //printPerson(list, (p) -> {return p.getGender() == Sex.MALE;});
+
+    //람다식의 body가 return문 하나만으로 구성되어 있으면 return 키워드, {}, 문장 끝의
+    //세미콜론(;)도 생략 할수있다
+    //printPerson(list, (p) -> p.getGender() == Sex.MALE);
+
+    // printPerson(list, (p)-> {
+    //   System.out.println(p.getName());
+    //   return p.getAge() >= 30;
+    // });
+
+    //람다식의 매개변수가 오직 하나일때는 매개변수를 둘러싸는 () 생략 할수 있다
+    // printPerson(list, p -> {
+    //   System.out.println(p.getName());
+    //   return p.getAge() >= 30;
+    // });
+
+    //람다식을 이용해서 printPerson 메소드를 호출하고
+    //그 결과로 나이 20에서 30살 사이의 여자만 출력되도록 구현 하라
+    printPerson(list, (p) -> {return p.getGender() == Sex.FEMALE && p.getAge() >= 20 && p.getAge() <= 30;});
   }
   
   //collection framework : List, Set, Map, Stack, Queue
@@ -37,11 +83,11 @@ public class test {
       }
     }
   }
+
   //List<Person> list, int low, int high
   //위 3개의 매개변수를 통해 Person 객체 리스트와, 나이의 범위(Low 이상, high 미만)를
   //입력 받아, 저 나이 범위의 Person 정보만 출력하는 메소드
   //printPersonWithinAgeRange를 구현
-
   public static void printPersonOlderThen(List<Person> list, int low, int high) {
     for (Person p : list) {
       if (p.getAge() >= low && p.getAge() <= high) {
@@ -81,3 +127,10 @@ class CheckPersonFemale10to20 implements CheckPerson {
     return p.getGender() == Sex.FEMALE && p.getAge() >= 10 && p.getAge() <= 20;
   }
 }
+
+class CheckPersonMale implements CheckPerson {
+  public boolean test(Person p) {
+    return p.getGender() == Sex.MALE;
+  }
+}
+
